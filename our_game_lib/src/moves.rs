4edc16644fa6574f0b569897073656a5de688bf0
Mathieu Ponal <mathieu.ponal@gmail.com>
2024-03-07 23:20:15 +0100

    formatted move properly

--- our_game_lib/src/moves.rs
+++ our_game_lib/src/moves.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(rustfmt, rustfmt_skip)]
+
 /// Represents a move in the game.
 /// Each move has an x and y coordinate.
 #[derive(PartialEq, Eq)]
@@ -9,35 +11,19 @@ pub struct Move {
 }
 
 // Aliases for different types of moves
-// MOVE_NW represents a move to the north-west
 pub const MOVE_NW: &Move = &Move { x: -1, y: 1 };
-// MOVE_N represents a move to the north
 pub const MOVE_N: &Move = &Move { x: 0, y: 1 };
-// MOVE_NE represents a move to the north-east
 pub const MOVE_NE: &Move = &Move { x: 1, y: 1 };
-// MOVE_W represents a move to the west
 pub const MOVE_W: &Move = &Move { x: -1, y: 0 };
-// MOVE_E represents a move to the east
 pub const MOVE_E: &Move = &Move { x: 1, y: 0 };
-// MOVE_SW represents a move to the south-west
 pub const MOVE_SW: &Move = &Move { x: -1, y: -1 };
-// MOVE_S represents a move to the south
 pub const MOVE_S: &Move = &Move { x: 0, y: -1 };
-// MOVE_SE represents a move to the south-east
 pub const MOVE_SE: &Move = &Move { x: 1, y: -1 };
 
 // Possible moves for each type of piece
-// MOVE_DUMMY is an empty array, representing no possible moves
 pub const MOVE_DUMMY: &[&Move] = &[];
-// MOVE_LION represents all possible moves for a Lion piece
-pub const MOVE_LION: &[&Move] = &[
-    MOVE_NW, MOVE_N, MOVE_NE, MOVE_W, MOVE_E, MOVE_SW, MOVE_S, MOVE_SE,
-];
-// MOVE_ELEPHANT represents all possible moves for an Elephant piece
-pub const MOVE_ELEPHANT: &[&Move] = &[MOVE_NW, MOVE_NE, MOVE_SW, MOVE_SE];
-// MOVE_GIRAFFE represents all possible moves for a Giraffe piece
-pub const MOVE_GIRAFFE: &[&Move] = &[MOVE_N, MOVE_W, MOVE_E, MOVE_S];
-// MOVE_CHICK represents all possible moves for a Chick piece
-pub const MOVE_CHICK: &[&Move] = &[MOVE_N];
-// MOVE_HEN represents all possible moves for a Hen piece
-pub const MOVE_HEN: &[&Move] = &[MOVE_NW, MOVE_N, MOVE_NE, MOVE_W, MOVE_E, MOVE_S];
+pub const MOVE_LION:     &[&Move] = &[MOVE_NW, MOVE_N, MOVE_NE, MOVE_W, MOVE_E, MOVE_SW, MOVE_S, MOVE_SE];
+pub const MOVE_ELEPHANT: &[&Move] = &[MOVE_NW,         MOVE_NE,                 MOVE_SW,         MOVE_SE];
+pub const MOVE_GIRAFFE: &[&Move] =  &[         MOVE_N,          MOVE_W, MOVE_E,          MOVE_S         ];
+pub const MOVE_CHICK:   &[&Move] =  &[         MOVE_N                                                   ];
+pub const MOVE_HEN:     &[&Move] =  &[MOVE_NW, MOVE_N, MOVE_NE, MOVE_W, MOVE_E,          MOVE_S         ];
