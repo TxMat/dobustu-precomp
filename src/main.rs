dabf8dfd7e7ef0bb6d0d43e41ad0ede3a4cceac4
Mathieu Ponal <mathieu.ponal@gmail.com>
2024-03-07 22:37:19 +0100

    added more rules (still rough code)

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,37 @@
 use game_helper::board::Board;
-use game_helper::moves::MOVE_N;
-use game_helper::piece::{Piece, PieceType, Color};
+use game_helper::piece::{Color, Piece, PieceType};
 
 fn main() {
-    
     let c = Piece::new(PieceType::Chick, Color::White);
     let g = Piece::new(PieceType::Giraffe, Color::White);
     let e = Piece::new(PieceType::Elephant, Color::White);
     let l = Piece::new(PieceType::Lion, Color::White);
     let h = Piece::new(PieceType::Hen, Color::White);
-    
+
     let C = Piece::new(PieceType::Chick, Color::Black);
     let G = Piece::new(PieceType::Giraffe, Color::Black);
     let E = Piece::new(PieceType::Elephant, Color::Black);
     let L = Piece::new(PieceType::Lion, Color::Black);
     let H = Piece::new(PieceType::Hen, Color::Black);
-    
+
     let white_pieces = vec![c, g, e, l, h];
     let black_pieces = vec![C, G, E, L, H];
-    
+
     let mut b = Board::init();
     while !b.is_game_over() {
         b.show();
         let random = rand::random::<usize>() % white_pieces.len();
         let random_m = rand::random::<usize>() % white_pieces[random].piece_type.moves().len();
-        b.move_piece(&white_pieces[random], white_pieces[random].piece_type.moves()[random_m]);
+        b.move_piece(
+            &white_pieces[random],
+            white_pieces[random].piece_type.moves()[random_m],
+        );
     }
     b.show();
     println!("Game Over");
     match b.winner {
         Some(Color::White) => println!("White wins"),
         Some(Color::Black) => println!("Black wins"),
-        None => println!("Draw")
+        None => println!("Draw"),
     }
 }
