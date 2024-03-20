39da96fdd473d797bfce38a0068f74c90c2d339f
Mathieu Ponal <mathieu.ponal@gmail.com>
2024-03-20 12:07:26 +0100

    almost working

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, vec};
 
 use log::info;
 
@@ -8,67 +8,95 @@ use game_helper_v2::piece::{Piece, HEN_2, LION_1};
 use game_helper_v2::structs::GameResult;
 use game_helper_v2::structs::Position::X0Y0;
 
-#[tokio::main(flavor = "multi_thread")]
-async fn main() {
+fn main() {
     env::set_var("RUST_LOG", "info");
     pretty_env_logger::init();
 
-    let b = Board::init();
+    let mut item_counts = vec![0, 0, 0];
+
+    let mut board_vec = vec![Board::init()];
+    let mut board_vec_temp: Vec<Board> = vec![];
+
+    let mut visited_white = std::collections::HashSet::new();
+    let mut visited_black = std::collections::HashSet::new();
+
+    let mut turn = true;
+
+    let mut depth = 0;
 
     //info!("{:?}", b.show_hex());
     //b.debug_show_board();
 
-    let mut vec_b = vec![];
-
-    b.debug_show_board_2();
-    let a = b.get_next_states(true);
-    match a {
-        GameResult::WhiteWin => {
-            info!("################################");
-            info!("################################");
-            info!("WW");
-        }
-        GameResult::BlackWin => {
-            info!("################################");
-            info!("################################");
-            info!("BW");
-        }
-        GameResult::Intermediate(brds) => {
-            info!("################################");
-            info!("################################");
-            info!("Intermediate");
-            for b in brds {
-                b.debug_show_board_2();
-                vec_b.push(b);
+    while !board_vec.is_empty() {
+        depth += 1;
+        let visited = match turn {
+            true => &mut visited_white,
+            false => &mut visited_black,
+        };
+        for board in board_vec {
+            if visited.contains(&board) {
+                continue;
             }
-        }
-    }
 
-    println!("########################################");
-    println!("############### DEPTH 2 ################");
-    println!("########################################");
-
-    for b in vec_b {
-        let a = b.get_next_states(false);
-        match a {
-            GameResult::WhiteWin => {
-                info!("################################");
-                info!("################################");
-                info!("WW");
-            }
-            GameResult::BlackWin => {
-                info!("################################");
-                info!("################################");
-                info!("BW");
-            }
-            GameResult::Intermediate(brds) => {
-                info!("################################");
-                info!("################################");
-                info!("Intermediate");
-                for b in brds {
-                    b.debug_show_board_2();
+            let r = match board.get_next_states(turn) {
+                GameResult::WhiteWin => 1,
+                GameResult::BlackWin => 0,
+                GameResult::Intermediate(bs) => {
+                    for b in bs {
+                        board_vec_temp.push(b)
+                    }
+                    -1
                 }
-            }
+            };
+
+            item_counts[(1 - r) as usize] += 1;
+
+            visited.insert(board);
         }
+
+        let total = item_counts[0] + item_counts[1] + item_counts[2];
+        info!(
+            "enumerating... (White Win: {}, Black Win: {}, Intermediate: {}, total: {}, depth: {})",
+            item_counts[0], item_counts[1], item_counts[2], total, depth
+        );
+
+        turn = !turn;
+        board_vec = board_vec_temp.clone();
+        board_vec_temp.clear();
     }
+
+    // while let Some(board) = board_vec.pop() {
+    //     let visited = match turn {
+    //         true => &mut visited_white,
+    //         false => &mut visited_black,
+    //     };
+    //     if visited.contains(&board) {
+    //         continue;
+    //     }
+    //
+    //     let r = match board.get_next_states(turn) {
+    //         GameResult::WhiteWin => {
+    //             1
+    //         },
+    //         GameResult::BlackWin => {
+    //             0
+    //         },
+    //         GameResult::Intermediate(bs) => {
+    //             for b in bs {
+    //                 board_vec.push(b)
+    //             }
+    //             -1
+    //         }
+    //     };
+    //
+    //     item_counts[(1 - r) as usize] += 1;
+    //     if visited.len() % 1000000 == 0 {
+    //         let total = item_counts[0] + item_counts[1] + item_counts[2];
+    //         info!("enumerating... (White Win: {}, Black Win: {}, Intermediate: {}, total: {})",
+    //             item_counts[0], item_counts[1], item_counts[2], total);
+    //     }
+    //
+    //     visited.insert(board);
+    //     turn = !turn;
+    // }
 }
